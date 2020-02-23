2a3c12e8c6d2bac4076d255bd39a9024b9f6933f
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-02-24 09:14:31 +1100

    Continue project 0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class NBody {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double T = scanner.nextDouble();
+        double dt = scanner.nextDouble();
+        String filename = scanner.next();
+        double radius = readRadius(filename);
+        Body[] bodyArray = readBodies(filename);
+    }
+
     // Get a double corresponding to the radius of the universe in that file
     public static double readRadius(String filename)
     {
@@ -7,6 +18,7 @@ public class NBody {
         return reader.readDouble();
     }
 
+    // return an array of Bodys corresponding to the bodies in the file
     public static Body[] readBodies(String filename)
     {
         In reader = new In(filename);
