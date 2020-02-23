cc487d764c0d8826dcd6d2bc9481430a03b5f9e6
Xiaocheng Fei <61fxc.job@gmail.com>
2020-02-23 21:12:45 +1100

    Finalised Body class

--- proj0/Body.java
+++ proj0/Body.java
@@ -41,4 +41,67 @@ public class Body {
         double distance = calcDistance(b);
         return g * mass * b.mass / (distance * distance);
     }
+
+    // returns the total force, these two methods describe the force exerted in the X direction
+    public double calcForceExertedByX(Body b)
+    {
+        double force = calcForceExertedBy(b);
+        double distance = calcDistance(b);
+        return force * (b.xxPos - xxPos) / distance;
+    }
+
+    // returns the total force, these two methods describe the force exerted in the Y direction
+    public double calcForceExertedByY(Body b)
+    {
+        double force = calcForceExertedBy(b);
+        double distance = calcDistance(b);
+        return force * (b.yyPos - yyPos) / distance;
+    }
+
+    //get the net X force exerted by all bodies in that array upon the current Body
+    public double calcNetForceExertedByX(Body[] b)
+    {
+        Body selfBody = this;
+        double netForceExertedByX = 0;
+        for(Body singleBody: b)
+        {
+            if(!singleBody.equals(selfBody))
+            {
+                netForceExertedByX += calcForceExertedByX(singleBody);
+            }
+        }
+        return netForceExertedByX;
+    }
+
+    // get the net Y force exerted by all bodies in that array upon the current Body
+    public double calcNetForceExertedByY(Body[] b)
+    {
+        Body selfBody = this;
+        double netForceExertedByY = 0;
+        for(Body singleBody: b)
+        {
+            if(!singleBody.equals(selfBody))
+            {
+                netForceExertedByY += calcForceExertedByY(singleBody);
+            }
+        }
+        return netForceExertedByY;
+    }
+
+    // determines how much the forces exerted on the body will cause that body to accelerate, and the resulting change in the body’s velocity and position in a small period of time
+    public void update(double dt, double fx, double fy)
+    {
+        // Calculate the accerlation using the provided x and y forces
+        double accerlerationX = fx / mass;
+        double accerlerationY = fy / mass;
+
+        // Calculate the new velocity by using the acceleration and current velocity
+        xxVel += dt * accerlerationX;
+        yyVel += dt * accerlerationY;
+
+        // update the positions by using the velocity and the current position
+        xxPos += dt * xxVel;
+        yyPos += dt * yyVel;
+    }
 }
+
