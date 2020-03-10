6bf09c84eeca00abbff9b43c452047a09684c4d5
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-03-10 17:38:25 +1100

    continue project 1A

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,18 @@
-public class LinkedListDeque {
+public class LinkedListDeque <T> {
+    public LinkedListDeque()
+    {
+
+    }
+
+    /* Create a deep copy of other */
+    public LinkedListDeque(LinkedListDeque other)
+    {
+
+    }
+
+    /* Get item using recursive method */
+    public T getRecursive(int index)
+    {
+
+    }
 }
