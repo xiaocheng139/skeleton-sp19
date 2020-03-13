0f51ecfbb3d487d648de0d33f99db10af253ea94
jackfei01 <48191365+jackfei01@users.noreply.github.com>
2020-03-13 11:38:07 +1100

    continue project 1A

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -4,50 +4,77 @@ public class ArrayDeque <T> {
     private T[] items;
     private int nextFirst;
     private int nextLast;
+    private static final int MINIMUMLENGTH = 8;
+    private static final double RFACTOR = 0.25;
 
     public ArrayDeque()
     {
-        items = (T[]) new Object[8];
+        items = (T[]) new Object[MINIMUMLENGTH];
         size = 0;
         nextFirst = 0;
-        nextLast = 7;
+        nextLast = MINIMUMLENGTH - 1;
     }
 
-    public void resize(int capacity)
+    public ArrayDeque(ArrayDeque other)
     {
-        T[] result = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, result, 0, size);
+        size = other.size();
+
+    }
+
+    public void resize()
+    {
+        /* Extent the array doubly */
+        if (size > items.length)
+        {
+            T[] result = (T[]) new Object[items.length * 2];
+            System.arraycopy(items, 0, result, 0, size);
+        }
+        /* Shrink the array, set the length to (capacity / RFACTOR) */
+        else if (items.length >= 16 && size < items.length * RFACTOR)
+        {
+            int newLength = (int) (size / RFACTOR);
+            if (newLength < MINIMUMLENGTH)
+            {
+                newLength = MINIMUMLENGTH;
+            }
+            T[] result = (T[]) new Object[newLength];
+            System.arraycopy(items, 0, result, 0, size);
+        }
     }
 
     /* Adds an item of type T to the front of the deque */
     public void addFirst(T item)
     {
-        items[nextFirst] = item;
         size ++;
+        resize();
+        items[nextFirst] = item;
         nextFirst = minusOne(nextFirst);
     }
 
     /* Adds an item of type T to the end of the deque */
     public void addLast(T item)
     {
-        items[nextLast] = item;
         size ++;
+        resize();
+        items[nextLast] = item;
         nextLast = plusOne(nextLast);
     }
 
     /* Removes and returns the item at the front of the deque. If no such item exists, returns null. */
     public T removeFirst()
     {
-        int preFirst = plusOne(nextFirst);
         size--;
+        resize();
+        int preFirst = plusOne(nextFirst);
         return items[preFirst];
     }
 
     /* Removes and returns the item at the back of the deque. If no such item exists, returns null. */
     public T removeLast()
     {
-        int preLast= minusOne(nextLast);
         size--;
+        resize();
+        int preLast= minusOne(nextLast);
         return items[preLast];
     }
 
@@ -77,15 +104,15 @@ public class ArrayDeque <T> {
         }
     }
 
-    /* Calculate the usage proportion of memory boxes */
-    public double getUsageRate()
+    /* Returns true if deque is empty, false otherwise */
+    public boolean isEmpty()
     {
-        return (float) size / items.length;
+        return size == 0;
     }
 
-    public int calculateRFactor()
+    /* Returns the number of items in the deque */
+    public int size()
     {
-        double usageRate = getUsageRate();
-
+        return size;
     }
 }
